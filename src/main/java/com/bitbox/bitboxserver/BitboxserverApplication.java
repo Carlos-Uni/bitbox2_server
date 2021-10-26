fec28360128026d2b0afe25643e730adc40de4fc
Carlos-Uni <43434753+Carlos-Uni@users.noreply.github.com>
2021-10-26 08:32:59 +0100

    Repository Initialization

---
+++ src/main/java/com/bitbox/bitboxserver/BitboxserverApplication.java
@@ -0,0 +1,13 @@
+package com.bitbox.bitboxserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BitboxserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BitboxserverApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bitbox/bitboxserver/BitboxserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bitbox.bitboxserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BitboxserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
